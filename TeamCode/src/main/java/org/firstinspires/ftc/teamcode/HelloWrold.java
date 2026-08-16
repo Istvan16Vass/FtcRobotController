cd38ca3ba53df034b8945be6f1345ffea1a2a883
Istvan16Vass <istvan.vass0416@gmail.com>
2026-08-16 21:52:43 +0300

    .

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWrold.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@Disabled
+@TeleOp
+public class HelloWrold extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData("hello","Wrold");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
