96da0fbaf5ca0b5104703428b025cea2de7e31fa
Istvan16Vass <istvan.vass0416@gmail.com>
2026-08-18 20:17:46 +0300

    Create VariableExamples.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariableExamples.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class VariableExamples extends OpMode {
+    @Override
+    public void init() {
+        int teamnumber=23014;
+        double motorSpeed=0.75;
+        boolean ClawClosed=true;
+        telemetry.addData("Team Number",teamnumber);
+        telemetry.addData("motor speed",motorSpeed);
+        telemetry.addData("Clav State",ClawClosed);
+    }
+
+
+    @Override
+    public void loop() {
+
+    }
+}
