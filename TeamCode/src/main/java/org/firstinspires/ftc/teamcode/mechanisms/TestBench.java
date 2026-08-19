98223365aedbf9aedd135691cd773f38eb6af83e
Istvan16Vass <istvan.vass0416@gmail.com>
2026-08-19 16:24:28 +0300

    Update TestBench.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/TestBench.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/TestBench.java
@@ -4,8 +4,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.teamcode.DCMotor;
-
 public class TestBench {
     // Touch Sensor
     private DigitalChannel touchSensor;
